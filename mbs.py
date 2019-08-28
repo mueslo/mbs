a2412728e4df42f1d9cea8344d452aba77ff4606
Johannes Falke <johannesfalke@googlemail.com>
2019-08-28 11:20:02 +0200

    Initial commit

---
+++ mbs.py
@@ -0,0 +1,92 @@
+import os
+import re
+import io
+import numpy as np
+import datetime
+import zipfile
+from contextlib import contextmanager
+from collections import OrderedDict
+from collections.abc import Iterable
+
+mbs_timestamp = lambda s: datetime.datetime.strptime(s, "%d/%m/%Y   %H:%M")
+frame_unit = datetime.timedelta(seconds=0.001)
+fname_re = re.compile(r'.*\d{5}_\d{5}.txt')
+
+
+
+def is_mbs_filename(path):     
+     fname = os.path.basename(path)
+     if fname_re.fullmatch(fname):
+         return True
+     return False
+
+
+def stats(metadata):
+    s = {}
+    if metadata['AcqMode'] == 'Fixed':
+        s['acqtime'] = metadata['ActScans'] * metadata['Frames Per Step'] * frame_unit
+        s['eff_acqtime'] = s['acqtime']
+    elif metadata['AcqMode'] == 'Swept':
+        s['acqtime'] = metadata['ActScans'] * metadata['TotSteps'] * metadata['Frames Per Step'] * frame_unit
+        s['eff_acqtime'] =  metadata['ActScans'] * metadata['No. Steps'] * metadata['Frames Per Step'] * frame_unit
+    elif metadata['AcqMode'] == 'Dither':
+        print('dither!')
+        s['acqtime'] = metadata['ActScans'] * metadata['TotSteps'] * metadata['Frames Per Step'] * frame_unit
+        s['eff_acqtime'] = metadata['ActScans'] * metadata['TotSteps'] * (metadata['No. Steps'] - metadata['TotSteps'])/metadata['No. Steps'] * metadata['Frames Per Step'] * frame_unit
+        
+    s['duration'] = metadata['TIMESTAMP:'] - metadata['STim']
+    return s
+
+
+@contextmanager
+def load(fname, zip_fname=None):
+    if zip_fname is None:
+        with open(fname, 'r') as f:
+            yield f
+    else:
+        with zipfile.ZipFile(zip_fname) as zip_f:
+            with zip_f.open(fname, 'r') as f:
+                with io.TextIOWrapper(f) as f:
+                    yield f
+
+
+def parse_data(fname, metadata_only=False, zip_fname=None):
+    with load(fname, zip_fname) as f:
+        data_flag = False
+        data = []
+        metadata = OrderedDict()
+        for line in f:
+            if data_flag:
+                data.append(list(map(float, line.split())))
+            elif line.startswith('DATA:'):
+                if metadata_only:
+                    return metadata
+                data_flag = True
+            else:
+                name, val = line.split('\t', 1)
+                val = val.strip()
+
+                for T in (int, float, mbs_timestamp):
+                    try:
+                        val = T(val)
+                        break
+                    except Exception as e:
+                        continue
+
+                metadata[name] = val
+        return np.array(data), metadata
+
+
+class MBSFilePathGenerator(object):
+    def __init__(self, prefix, directory):
+        self.prefix = prefix
+        self.directory = directory
+    
+    def __call__(self, number, region):
+        if isinstance(number, Iterable):
+            return [self(n, region) for n in number]
+        if isinstance(region, Iterable):
+            return [self(number, r) for r in region]
+        
+        fname = "{}{:05d}_{:05d}.txt".format(self.prefix, number, region)
+        return os.path.join(self.directory, fname)
