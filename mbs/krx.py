1812a82c1fdeb6b01f3aba5ee00793d59d769701
Johannes Falke <johannesfalke@googlemail.com>
2022-07-18 14:40:54 +0200

    update KRXFile to allow loading from gzipped files and zip archives

--- mbs/krx.py
+++ mbs/krx.py
@@ -1,31 +1,34 @@
+import io
+import struct
 import numpy as np
-i32_nbytes = np.dtype(np.int32).itemsize
+
+from .io import load
+
 
 class KRXFile(object):
-    def __init__(self, fname):
+    def __init__(self, fname, zip_fname=None):
         self.fname = fname
-        self.header_dtype = self.get_header_dtype() # todo use py38+ @cached_property
-        self.read_header()
+        self.zip_fname = zip_fname
+        self._read_header()
 
     def __repr__(self):
         return f"KRXFile({self.fname})"
-        
-    def get_header_dtype(self):
-        dt_test = np.fromfile(self.fname, dtype=np.int32, count=2)
-        if np.all(dt_test != 0):
-            return np.int32
-        else:
-            return np.int64
     
-    def read_header(self):
-        with open(self.fname, 'rb') as f:
-            hdr0 = np.fromfile(f, dtype=self.header_dtype, count=1)[0]
-            hdr1 = np.fromfile(f, dtype=self.header_dtype, count=hdr0)
-            hdr2 = np.fromfile(f, dtype=self.header_dtype, count=2)
+    def _read_header(self):
+        with load(self.fname, zip_fname=self.zip_fname, mode='rb') as f:
+            dt_test = np.array(struct.unpack('ll', f.read(2 * 4)))
+            if all(dt_test != 0):
+                dt = np.dtype('int32')
+            else:
+                dt = np.dtype('int64')
+            f.seek(0)
+            hdr0 = struct.unpack(dt.char, f.read(dt.itemsize))[0]
+            hdr1 = struct.unpack(dt.char * hdr0, f.read(dt.itemsize * hdr0))
+            hdr2 = struct.unpack(dt.char * 2, f.read(dt.itemsize * 2))
 
             if hdr2[0] != 0:
                 # DimSize+Len+MSA MapSizeArray
-                map_size_arr = np.fromfile(f, dtype=self.header_dtype, count=hdr2[1])
+                map_size_arr = struct.unpack(dt.char * hdr2[1], f.read(dt.itemsize * hdr2[1]))
                 no_y = map_size_arr[0]
                 no_e = map_size_arr[1]
                 self.map_size = map_size_arr[2:]
@@ -33,10 +36,10 @@ class KRXFile(object):
                 # old KRX without dimension info MapSizeArray
                 no_y = hdr1[1]
                 no_e = hdr1[2]
-                self.map_size = np.array([len(hdr1)//3])
+                self.map_size = np.array([hdr0//3])
         
-        assert np.prod(self.map_size) == len(hdr1)//3
-        hdr1 = hdr1.reshape(-1, 3)
+        assert np.prod(self.map_size) == hdr0//3
+        hdr1 = np.array(hdr1).reshape(-1, 3)
         self.page_start = hdr1[:, 0]
         self.page_shape = hdr1[:, 1:]
 
@@ -45,20 +48,30 @@ class KRXFile(object):
         return np.prod(self.map_size)
     
     def page(self, n=0):
+        dt = np.dtype('int32')
         assert 0 <= n < self.num_pages
-        return np.memmap(self.fname, mode='r', 
-                         dtype=np.int32, offset=self.page_start[n]*i32_nbytes, 
-                         shape=tuple(self.page_shape[n, ::-1]), order='F')
+        with load(self.fname, zip_fname=self.zip_fname, mode='rb') as f:
+            if isinstance(f, io.BufferedReader):
+                # mmap normal files
+                return np.memmap(f, mode='r', dtype=dt,
+                                 offset=self.page_start[n] * dt.itemsize,
+                                 shape=tuple(self.page_shape[n, ::-1]), order='F')
+            else:
+                # for zipped, gzipped, files etc. load data into memory
+                f.seek(self.page_start[n] * dt.itemsize)
+                nbytes = dt.itemsize * np.prod(self.page_shape[n, ::-1])
+                return np.frombuffer(f.read(nbytes), dtype=dt).reshape(tuple(self.page_shape[n])).T
 
     def page_metadata(self, n=0):
+        dt = np.dtype('int32')
         assert 0 <= n < self.num_pages
         page_size = self.page_start[n] + np.prod(self.page_shape[n])
-        with open(self.fname, 'rb') as f:
-            f.seek(page_size*i32_nbytes)
-            hdr_len = np.fromfile(f, dtype=np.int32, count=1)[0]
+        with load(self.fname, zip_fname=self.zip_fname, mode='rb') as f:
+            f.seek(page_size * dt.itemsize)
+            hdr_len = struct.unpack(dt.char, f.read(dt.itemsize))[0]
             return f.read(hdr_len).decode('utf8')
     
     def export_page_txt(self, out_fname, n=0):
         with open(out_fname, 'w', newline='') as f:
-                f.write(self.page_metadata(n))
-                np.savetxt(f, self.page(n), delimiter='\t', fmt="%d")
+            f.write(self.page_metadata(n))
+            np.savetxt(f, self.page(n), delimiter='\t', fmt="%d")
